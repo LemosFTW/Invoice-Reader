c319b6c07269bec47d3f53061ca9fc29085db8a6
Rodrigo <rlemosfernandes1@gmail.com>
2024-07-04 09:39:32 +0100

    Adding invoices sumary and list of invoices

--- src/app/dashboard/invoices/page.tsx
+++ src/app/dashboard/invoices/page.tsx
@@ -3,24 +3,101 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation"; // Import from next/navigation
 import { useEffect, useState } from "react";
-import SideBar from "@/components/sideBar";
+import axios from "axios";
 
 export default function Dashboard() {
   const { data: session, status } = useSession();
-  const [showSidebar, setShowSidebar] = useState(false);
+  const [invoices, setInvoices] = useState([
+    { id: 0, filename: "", extractedText: "" },
+  ]);
+  const [editModalOpen, setEditModalOpen] = useState(false);
   const router = useRouter();
 
   useEffect(() => {
-    if (status !== "loading" && !session) {
-      router.push("/");
-    }
+    if (status !== "loading" && !session) router.push("/");
   }, [session, status, router]);
 
   if (status === "loading") return <p>Loading...</p>;
 
+  useEffect(() => {
+    fetchInvoices();
+  }, []);
+
+  const fetchInvoices = async () => {
+    let userEmail = "";
+
+    if (session?.user !== (null || undefined))
+      userEmail = session.user.email as string;
+
+    await axios
+      .post("http://localhost:8000/invoices", { email: userEmail })
+      .then((response) => {
+        if (response.status === 201) {
+          console.log("Invoices fetched successfully");
+          setInvoices(response.data);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching invoices:", error);
+      });
+  };
+
+  const deleteInvoice = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    let id = e.currentTarget.id;
+    let userEmail = session?.user?.email as string;
+    await axios
+      .delete(`http://localhost:8000/invoices/${id}`, {
+        data: { email: userEmail },
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          console.log("Invoice deleted successfully");
+          fetchInvoices();
+        }
+      })
+      .catch((error) => {
+        console.error("Error deleting invoice:", error);
+      });
+  };
+
+  const editInvoice = async (e: React.MouseEvent<HTMLButtonElement>) => {};
+
   return (
-    <div className="">
-        invoices
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Your Invoices Dashboard</h1>
+      {/* print invoices: invoices.filename , invoices.extractedText, invoices.id */}
+      {invoices.map((invoice) => (
+        <div
+          key={invoice.id}
+          className="flex flex-row justify-between items-center p-4 mb-4 border border-gray-300 rounded-lg shadow-sm bg-slate-400 hover:bg-slate-500 transition duration-300 ease-in-out"
+        >
+          <div className="flex flex-row items-center">
+            <div className="ml-4">
+              <span className="text-lg font-semibold text-gray-700">
+                ID: {invoice.id}
+              </span>
+              <p className="text-lg font-semibold text-gray-900">
+                {invoice.filename}
+              </p>
+              <p className="text-sm text-gray-600">{invoice.extractedText}</p>
+            </div>
+          </div>
+          <div className="flex flex-row space-x-2">
+            <button
+              className="text-white bg-blue-500 border border-blue-500 p-2 rounded-md hover:bg-blue-600"
+              onClick={(e) => editInvoice(e)}
+            >
+              Edit
+            </button>
+            <button
+              className="text-white bg-red-500 border border-red-500 p-2 rounded-md hover:bg-red-600"
+              onClick={(e) => deleteInvoice(e)}
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/app/dashboard/upload/page.tsx
+++ src/app/dashboard/upload/page.tsx
@@ -1,17 +1,55 @@
 "use client";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation"; // Import from next/navigation
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import SideBar from "@/components/sideBar";
 import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Tesseract from "tesseract.js";
+import {OpenAI,AzureOpenAI,ClientOptions} from "openai";
 
-export default function upload() {
+export default function Upload() {
   const [file, setFile] = useState<File>();
-
   const { data: session, status } = useSession();
   const [showSidebar, setShowSidebar] = useState(false);
+  const [lines, setLines] = useState([]); // [ {text: "line1"}, {text: "line2"}]
+  const [content, setContent] = useState('');
+  const [summary, setSummary] = useState('');
   const router = useRouter();
+
+  const readImageContent = async (image: any) => {
+    let data = await Tesseract.recognize(image, 'por');
+    setLines(data.lines as any);
+    // setContent(data.text);
+    var contentParse  = data.lines.map((line: any) => line.text).join('\n');
+    // console.log(contentParse)
+    setContent(contentParse);
+
+    // summarizeContent(data.text);
+  }
+
+  //Summarize the content using OpenAI
+  const summarizeContent = async (text: string) => {
+    try {
+        const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY , dangerouslyAllowBrowser: true});
+  
+        const response = await openai.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          messages: [
+            { role: "system", content: "Você é um assistente que ajuda a resumir textos." },
+            { role: "user", content: `Faça um resumo detalhado do seguinte texto em português e divida em tópicos principais: ${text}` }
+          ],
+          max_tokens: 150,
+        });
+
+      if (response.choices && response.choices.length > 0) 
+        response.choices[0].message.content != null ? setSummary(response.choices[0].message.content) : setSummary('No summary found');
+      
+    } catch (error) {
+      console.error("Error summarizing content:", error);
+    }
+  };
 
   useEffect(() => {
     if (status !== "loading" && !session) {
@@ -28,24 +66,50 @@ export default function upload() {
     try {
       const data = new FormData();
       data.set("file", file);
+      data.set("userEmail", session?.user?.email as string);
 
-        await axios.post("http://localhost:8000/upload", data)
-        .then((res) => { console.log(res.status); console.log(res.data);})
-        .catch((e) => { console.error(e); });
+      await axios
+        .post("http://localhost:8000/upload", data)
+        .then((res) => {
+          let url = URL.createObjectURL(file);
+          if (res.status === 201)
+            readImageContent(url);
+        })
+        .catch((e) => {
+          console.error(e);
+        });
 
-        
-      }catch (e) {
+
+        var updateData = {
+          fileName: file.name,
+          userEmail: session?.user?.email as string,
+          //content: summary
+          content: content
+        }
+        await axios.patch('http://localhost:8000/upload', updateData )
+        .then((res) => {
+          if (res.status === 200)
+            toast.success("File updated successfully");
+
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+
+
+
+
+    } catch (e) {
       console.error(e);
-      }
-    };
+    }
+  };
 
   return (
     <>
       <div>
-
-        <h1 
-        className=" text-4xl font-bold p-5 mx-12 text-center"
-        >Upload a new Invoice</h1>
+        <h1 className=" text-4xl font-bold p-5 mx-12 text-center">
+          Upload a new Invoice
+        </h1>
       </div>
 
       <form
@@ -63,11 +127,26 @@ export default function upload() {
             e.preventDefault();
           }}
         />
-        
-        <input type="submit" value="Upload" 
-        className="bg-blue-500 hover:bg-blue-700 text-white font-bold m-12 py-2 px-4 rounded"/>
-        
+
+        <input
+          type="submit"
+          value="Upload"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold m-12 py-2 px-4 rounded"
+        />
       </form>
+      <div>
+        <div className="flex gap-x-[30px] mt-20">
+          <div>
+            <h2>Summary</h2>
+            {summary && <p>{summary}</p>}
+            {content && <p>{content}</p>}
+          </div>
+          {/* <h2>Content</h2>
+            {lines.map((line, index) => (
+              <p key={index}>{line}</p>
+            ))} */}
+        </div>
+      </div>
     </>
   );
 }
